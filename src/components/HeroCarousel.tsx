05181907e8d3fee51c58b94daa5ebd46eb00f13b
moharm <youssef.moharm.2024@aiu.edu.eg>
2026-09-06 15:47:51 +0300

    fix: move headline further down - increase top padding

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -127,8 +127,8 @@ export default function HeroCarousel() {
 
       {/* Content - Left Side */}
       <div className="absolute inset-0 flex flex-col justify-end p-8 md:p-12 pb-1/3">
-        {/* Headline (Left side) - positioned lower */}
-        <div className="z-10 max-w-lg mb-auto">
+        {/* Headline (Left side) - positioned much lower */}
+        <div className="z-10 max-w-lg mb-auto pt-96">
           <h1
             key={`headline-${currentSlide}`}
             className={`nv-heading font-black text-6xl md:text-7xl lg:text-8xl leading-none mb-6 transition-all duration-700 ease-out whitespace-pre-line ${
